f99c9ce5da13a38ddc8081e716f180abfc276f38
LavanyaVaidya <labulavanya@gmail.com>
2025-12-13 14:48:02 +0530

    Optimised DFS DAY11 Part1

--- DAY11/part1.py
+++ DAY11/part1.py
@@ -577,13 +577,17 @@ dvu: icp qxh hvb
 """
 import collections as c
 
-def dfs(adj: dict, current: str, target: str) -> int:
+def dfs(adj: dict, current: str, target: str, visited: set) -> int:
     if current == target:
         return 1
     count = 0
-    # visited.add(current)
+    visited.add(current)
     for neigh in adj.get(current, []):
-        count += dfs(adj,neigh,target)
+        if neigh not in visited:
+            count += dfs(adj,neigh,target,visited)
+        else:
+            count += mp[neigh]
+    mp[current] = count
     return count
 
 matrix = [x.split(':') for x in input.strip().splitlines()]
@@ -591,11 +595,10 @@ matrix = [x.split(':') for x in input.strip().splitlines()]
 start_node = [row[0] for row in matrix]
 other_nodes = [row[1].strip().split(" ") for row in matrix]
 
-adj = c.defaultdict(list)
-
+adj, mp = c.defaultdict(list), c.defaultdict(lambda : -1)
+visited = set()
 for i in range(len(start_node)):
     adj[start_node[i]] = other_nodes[i]
-
-ans = dfs(adj,'you','out')
+ans = dfs(adj,'you','out',visited)
 
 print(ans)
